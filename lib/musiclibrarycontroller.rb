ae80801308748bc8eeeed3e0921cb7d467565061
Elle Wilson <eleanorwilsonn@gmail.com>
2018-03-13 20:37:43 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -58,7 +58,7 @@ end
 
     if genre = Genre.find_by_name(input)
       genre.songs.sort{|a, b| a.name <=> b.name}.each.with_index(1) do |s, i|
-        puts "#{i}. #{s.genre.name} - #{s.name}"
+        puts "#{i}. #{s.artist.name} - #{s.name}"
       end
     end
 end
